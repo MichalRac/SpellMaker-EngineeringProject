755829564ff5b73f369e2ab520cd7bdfd1ab525d
MichalRac <rac.michalmail@gmail.com>
2021-01-10 17:44:14 +0100

    Update animation events

---
+++ SpellMaker/Assets/Scripts/Features/Ingame/ActionEffects/ActionEffectType.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public abstract class ActionEffect : IActionEffect
+{
+    public int TurnsLeftAffected { get; set; }
+    public int Power { get; }
+
+    protected ActionEffect(int turnLenght, int power)
+    {
+        TurnsLeftAffected = TurnsLeftAffected;
+        Power = power;
+    }
+
+    // Happens on turn finished
+    public virtual void Affect(Unit unitAffected)
+    {
+        TurnsLeftAffected--;
+    }
+
+    public bool IsFinished() => TurnsLeftAffected <= 0;
+
+}
---
+++ SpellMaker/Assets/Scripts/Features/Ingame/ActionEffects/IActionEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IActionEffect
+{
+    int TurnsLeftAffected { get; set; }
+
+    int Power { get; }
+
+    void Affect(Unit unitToAffect);
+    bool IsFinished();
+}
